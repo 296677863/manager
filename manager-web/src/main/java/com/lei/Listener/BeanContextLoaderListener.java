c8bd24277cb39f0075bd52ba07859ef3330002d9
lei <296677863@qq.com>
2016-11-30 15:09:25 +0800

    Test

---
+++ manager-web/src/main/java/com/lei/Listener/BeanContextLoaderListener.java
@@ -0,0 +1,21 @@
+package com.lei.Listener;
+
+import com.lei.util.BeanProvider;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+
+/**
+ * Created by lei on 11/30/2016.
+ */
+public class BeanContextLoaderListener extends ContextLoaderListener{
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        super.contextInitialized(event);
+        WebApplicationContext applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(event.getServletContext());
+        BeanProvider.initialize(applicationContext);
+    }
+}
